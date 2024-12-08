8d3259dd3f259f098d2d58722474dfa03b281fbf
Abdurazaq333 <irazaqr986@gmail.com>
2024-12-08 11:50:57 +0500

    add sticky scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import AboutUs from "@/components/AboutUs";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <AboutUs />
     </div>
   );
 }
